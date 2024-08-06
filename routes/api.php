92f09bc913045d9762d6de5a1db326cbb4d159d4
= <jianraphael@qualitytrade.com>
2024-08-07 07:41:08 +0800

    PRACTICE COMMIT - Episode 2 and 3

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,34 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'login']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-Route::get('/user', function (Request $request) {
+// http://localhost:8000/api/
+// univseral resource locator
+// tickets
+// users
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function() {
+    return Ticket::all();
+});
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
