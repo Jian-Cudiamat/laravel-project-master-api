19ab46aae83b9b43517ff239965b5637252318d6
= <jianraphael@qualitytrade.com>
2024-08-07 10:28:56 +0800

    PRACTICE COMMIT - Episode 14

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -7,12 +7,11 @@ use App\Http\Filters\V1\TicketFilter;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
-use App\Traits\ApiResponses;
 
-class AuthorTicketsController extends Controller
+class AuthorTicketsController extends ApiController
 {
-    use ApiResponses;
     public function index($author_id, TicketFilter $filters) {
         return TicketResource::collection(
             Ticket::where('user_id', $author_id)->filter($filters)->paginate()
@@ -32,5 +31,25 @@ class AuthorTicketsController extends Controller
         ];
 
         return new TicketResource(Ticket::create($model));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($author_id, $ticket_id)
+    {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            if ($ticket->user_id == $author_id) {
+                $ticket->delete();
+                return $this->ok('Ticket successfully deleted');
+            }
+
+            return $this->error('Ticket cannot found.', 404);
+
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket cannot found.', 404);
+        }
     }
 }
